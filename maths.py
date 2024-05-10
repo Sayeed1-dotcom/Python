cb17b4bfa69d0aa53c5d27a99171c6e7f4bca2bf
Sayeed Mohammad <toothpaste744@gmail.com>
2024-05-11 02:43:10 +0800

    updated maths.py

--- maths.py
+++ maths.py
@@ -1,3 +1,4 @@
+#arithmatic operators (+,-,*,/,%,**)
 a=4
 b=3
 c=5
@@ -18,3 +19,52 @@ print(M)
 #Division
 D=d/c
 print(D)
+
+#remainder
+R=a % b
+print(R)
+
+#power like a^2=4^2=16
+P=a ** b
+print(P)
+
+#relational operators (==,!=,>,>=,<,<=)
+
+a=10
+b=20
+
+print(a == b) #Answer will be False
+print(a != b) #Answer will be True
+print(a > b) #Answer will be False
+print(a >= b) #Answer will be False
+print(a < b) #Answer will be True
+print(a <= b) #Answer will be True
+
+#Assignment operators(+=,-=,*=,/=,%=,**=)
+
+i = 10
+i += 5 #output 10+5=15
+print("i =", i)
+
+j=12
+j -= 5 #output 12-5=7
+print("j =", j)
+
+k=3
+k *= 5 #output 3*5=15
+print("k =", k)
+
+l=10
+l /= 5 #output 10/5=2
+print("l =", l)
+
+p=10
+p %= 5 #output 10%5=0
+print("p =", p)
+
+q=2
+q **= 3 #output 2**3=8
+print("q =", q)
+
+
+#Logical operators (not,and,or)
