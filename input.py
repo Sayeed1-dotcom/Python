3c1e7dd71e08b9fbeb012fa901d5be475566012a
Sayeed Mohammad <toothpaste744@gmail.com>
2024-05-13 02:20:46 +0800

    added input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+name = str(input("Enter your name:"))
+fullname = str(input ("Full name:"))
+age = int(input("Enter your age:"))
+number = int(input("Enter your mobile number:"))
+
+print("Welcome,", name)
+print("Full name :", fullname)
+print("You entered age is :", age)
+print("Your mobile number is :", number)
+
