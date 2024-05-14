305e1d11054680ae6bea0e3f957837ae1bd154b2
Sayeed Mohammad <toothpaste744@gmail.com>
2024-05-15 04:29:19 +0800

    modified calculator.py

--- mini project/calculator.py
+++ mini project/calculator.py
@@ -22,3 +22,37 @@ elif operator == "**":
 
 else:
     print("Invalid operation")
+     
+
+             #OR 
+             
+# #a = float(input("Enter 1st number :"))
+# operator = input("Enter(+,-,*,/,%,**) :")
+# b = float(input("Enter 2nd number :"))
+
+# sum = a + b
+# diff = a - b
+# M = a * b
+# A = a / b
+# R = a % b
+# P = a ** b
+# if operator == "+":
+#     print("The result is :", sum)
+
+# elif operator == "-":
+#     print("The result is :", diff)
+
+# elif operator == "*":
+#     print("The result is :", M)
+
+# elif operator == "/":
+#     print("The result is :", A)
+
+# elif operator == "%":
+#     print("The result is :", R)
+
+# elif operator == "**":
+#     print("The result is :", P)
+
+# else:
+#     print("Invalid operation") 
