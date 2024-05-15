1399b363522c824e8740b04e6d614e9f012f29f1
Sayeed Mohammad <toothpaste744@gmail.com>
2024-05-16 03:11:01 +0800

    added loops folder and list.py file

---
+++ list.py
@@ -0,0 +1,11 @@
+marks = [95,96,97,98,"math"]
+
+print(marks)
+print(marks[0]) #it will print 0th index position number: 95
+print(marks[1]) #it will print 2nd position number: 96
+print(marks[0:2]) #it will print 1st two number: 95,96
+print(marks[2:4]) #it will print 3rd and 4th number: 97,98
+
+ # if we want to print number from back
+print(marks[-1]) # it will print 'math'
+print(marks[-2]) # it will print '98'
---
+++ loops/for.py
@@ -0,0 +1,14 @@
+for serial in range(5):
+    print(serial) # it will print 0 to 4
+
+for serial in range(5):
+    print(serial  * '*')
+
+for serial in range(5):
+    print(serial + 1) # it will print 1 to 5.
+    
+            # for loop in list
+marks = [95,96,97,98]
+
+for result in marks :
+    print(result)
