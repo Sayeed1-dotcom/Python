5bb7b12a37ee3cf2587a7c43304337561e449974
Sayeed Mohammad <toothpaste744@gmail.com>
2024-05-19 06:02:27 +0800

    updated list.py

--- list.py
+++ list.py
@@ -9,3 +9,38 @@ print(marks[2:4]) #it will print 3rd and 4th number: 97,98
  # if we want to print number from back
 print(marks[-1]) # it will print 'math'
 print(marks[-2]) # it will print '98'
+
+#Checking length of list
+print(len(marks))
+
+#Append(means adding in list)
+marks.append(85)
+print(marks)
+
+#insert (means adding in specific position on the list)
+marks.insert(0, 70)
+marks.insert(1, 80)
+
+print(marks)
+
+#number existing check on the list
+print(95 in marks) #True
+print(90 in marks) #False
+
+#for loop in list
+name = ["Ahnat","Afrin","Tanha"]
+for student in name:
+    print(student)
+print(len(name))
+
+#while loop in list
+fruits = ["apple","mango","orange"]
+
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i+=1
+
+#to clean list
+fruits.clear()
+print(fruits)
--- loops/for.py
+++ loops/for.py
@@ -2,7 +2,7 @@ for serial in range(5):
     print(serial) # it will print 0 to 4
 
 for serial in range(5):
-    print(serial  * '*')
+    print(serial * '*')
 
 for serial in range(5):
     print(serial + 1) # it will print 1 to 5.
